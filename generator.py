503bdf285ace3a38357c42284a7c21c1b7431f83
justNoriX <osur@onet.pl>
2024-10-14 14:46:52 +0200

    Chyba final version

---
+++ generator.py
@@ -0,0 +1,15 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+
+# Funkcja do wygenerowania klucza
+def generate_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password)
+    return key
